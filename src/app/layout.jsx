3ecd78bfab6edda5bfc36ed00636b6f53d1ab7e5
butkus13 <kimberlymdyer@gmail.com>
2024-11-01 18:07:56 -0400

    Initial project source code

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,30 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+//import { Inter } from "next/font/google";
+//import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+//const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "MO-PCCO.R3 - NextJS Financial Dashboard",
-  description: "Starter code for Module 25 Capstone project - Financial Dashbard",
-};
+//export const metadata = {
+//  title: "MO-PCCO.R3 - NextJS Financial Dashboard",
+//  description: "Starter code for Module 25 Capstone project - Financial Dashbard",
+//};
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+//export default function RootLayout({ children }) {
+//  return (
+//    <html lang="en">
+//      <body className={inter.className}>{children}</body>
+//    </html>
+//  );
+//}
+
+import '@/app/ui/global.css'; // Import global styles
+import { inter } from '@/app/ui/fonts'; // Import the Inter font
+
+export default function RootLayout({
+    children,
+}) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
